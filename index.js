1da907c4018a64b02b655fcdd926df3907b2d3d3
Davide Giliberti <gilo.davide@hotmail.it>
2024-06-11 16:54:51 +0200

    primo esercizio completato

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let contatore = sessionStorage.getItem("counter") || 0;
+//esercizio contatore
 
+let contatore = sessionStorage.getItem("counter") || 0;
 function incrementa() {
   contatore++;
   document.getElementById("timer").textContent = contatore;
@@ -14,6 +15,8 @@ const intervallo = setInterval(incrementaEStampa, 1000);
 
 document.getElementById("timer").textContent = contatore;
 
+//esercizio stampa nome
+
 function salva() {
   const nome = document.getElementById("testo").value;
   localStorage.setItem("salvato", nome);
@@ -27,5 +30,15 @@ function stampaNome() {
   const prendiNome = localStorage.getItem("salvato");
   nomeSalvato.textContent = prendiNome;
 }
+
+function cancellaNome() {
+  const cancellaDato = localStorage.removeItem("salvato");
+  const cancellaStampa = document.getElementById("nomeSalvato");
+  cancellaStampa.textContent = "";
+}
+
+const elimina = document.getElementById("delete");
+
+elimina.addEventListener("click", cancellaNome);
 
 stampaNome();
